efdd30256a9f23af102f51c069a1e8c1d5b36d8e
user <user@users.noreply.github.com>
2024-04-15 23:34:25 +1000

    home border

--- src/pages/layouts/projects/components/Welcome.tsx
+++ src/pages/layouts/projects/components/Welcome.tsx
@@ -8,17 +8,12 @@ interface projectProps {
 
 const Welcome: React.FC<projectProps> = ({ title, menu }) => {
   return (
-    <div className="flex w-full h-[10vh] bg-white border-b border-black items-center justify-center">
-      <div className="w-[93vw] flex justify-between">
-        <div>
+    <div className="flex w-full h-[10vh] border-b border-black items-center">
+        <div className="ml-[2vh]">
           <Link href="/projects" className="no-underline">
             <h1 className="text-3xl text-black">{title}</h1>
           </Link>
         </div>
-        <div>
-          <h1 className="text-3xl text-black">{menu}</h1>
-        </div>
-      </div>
     </div>
   );
 };
