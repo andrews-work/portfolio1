97535820e8b009d1c8c18ae7e6881a2e29114eb9
user <user@users.noreply.github.com>
2024-04-15 20:21:41 +1000

    tablet home

--- src/pages/layouts/home/components/InfoT.tsx
+++ src/pages/layouts/home/components/InfoT.tsx
@@ -4,48 +4,46 @@ import { info } from '../../../../components/labels';
 
 const InfoM = () => {
   return (
-    <div className="w-full flex flex-col space-between">
-      <div className="w-full">
-        <div className="mt-[5vh] flex flex-col justify-center ml-[5vw]">
+    <div className="h-[90vh] w-full flex flex-col justify-between">
+      <div className="h-[80vh] w-full flex flex-col space-y-8">
+        <div className="flex-1">
+          <div className="flex flex-col justify-center ml-[5vw]">
             <h1 className="font-bold text-black text-3xl mb-[1vh]">{info.about}</h1>
             <div className="flex">
-                <h4 className="text-xl mb-[1vh]">1. </h4>
-                <Link href="/websites" className="text-black underline text-xl ml-[1vw]">
-                <h4> {info.websites}</h4>
-                </Link>
+              <h4 className="text-xl mb-[1vh]">1. </h4>
+              <Link href="/websites" className="text-black underline text-xl ml-[1vw]">
+                <h4>{info.websites}</h4>
+              </Link>
             </div>
             <div className="flex">
-                <h4 className="text-xl mb-[1vh]">2. </h4>
-                <Link href="/projects" className="text-black underline text-xl ml-[1vw]">
+              <h4 className="text-xl mb-[1vh]">2. </h4>
+              <Link href="/projects" className="text-black underline text-xl ml-[1vw]">
                 <h4>{info.projects}</h4>
-                </Link>
+              </Link>
             </div>
             <div className="flex">
-                <h4 className="text-xl mb-[1vh]">3. </h4>
-                <Link href="/languages" className="text-black underline text-xl ml-[1vw]">
+              <h4 className="text-xl mb-[1vh]">3. </h4>
+              <Link href="/languages" className="text-black underline text-xl ml-[1vw]">
                 <h4>{info.languages}</h4>
-                </Link>
+              </Link>
             </div>
             <div className="flex">
-                <h4 className="text-xl mb-[1vh]">4. </h4>
-                <Link href="/store/" className="text-black underline text-xl ml-[1vw]">
+              <h4 className="text-xl mb-[1vh]">4. </h4>
+              <Link href="/store/" className="text-black underline text-xl ml-[1vw]">
                 <h4>{info.store}</h4>
-                </Link>
+              </Link>
             </div>
+          </div>
         </div>
-      </div>
-      <div className="w-full">
-        <div className="mt-[5vh] flex flex-col justify-center">
-          <div className="ml-[5vw] mb-[1vh]">
+        <div className="flex-1">
+          <div className="flex flex-col justify-center ml-[5vw]">
             <h1 className="font-bold text-black text-3xl mb-[1vh]">{info.reviews}</h1>
             <h4 className="text-xl">{info.first}</h4>
             <h4 className="text-xl">{info.second}</h4>
           </div>
         </div>
-      </div>
-      <div className="w-full">
-        <div className="mt-[5vh] flex flex-col justify-center">
-          <div className="ml-[5vw] mb-[1vh]">
+        <div className="flex-1">
+          <div className="flex flex-col justify-center ml-[5vw]">
             <h1 className="font-bold text-black text-3xl mb-[1vh]">{info.contact}</h1>
             <h4 className="text-xl">{info.email}</h4>
           </div>
