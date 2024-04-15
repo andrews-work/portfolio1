d51dfdade88e55126e44177dbfdf1c8b0e85691c
user <user@users.noreply.github.com>
2024-04-15 19:09:33 +1000

    add logo please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import HomePage from '@/pages/layouts/home/HomePage';
 import './globals.css';
+import Head from 'next/head';
 import './styles.css';
 
 const Page = () => {
-  return <HomePage />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HomePage />
+    </>
+  );
 };
 
 export default Page;
