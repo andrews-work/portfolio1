32a32864f54b9aac5f36da65dc597c94a4e41baf
user <user@users.noreply.github.com>
2024-04-15 20:39:41 +1000

    tablet home

--- src/pages/layouts/home/components/WelcomeM.tsx
+++ src/pages/layouts/home/components/WelcomeM.tsx
@@ -4,7 +4,7 @@ import { welcome } from '../../../../components/labels';
 
 const WelcomeM = () => {
   return (
-    <div className="flex w-full h-[10vh] border-b border-black">
+    <div className="flex w-full h-[10vh] border-b border-black items-center">
         <div className="ml-[2vh]">
           <Link href="/" className="no-underline">
             <h1 className="text-3xl text-black">{welcome.title}</h1>
