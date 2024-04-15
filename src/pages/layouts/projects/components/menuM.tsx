fcb8c4d0eec556173dd68a8c0ab160aad217b240
user <user@users.noreply.github.com>
2024-04-15 19:48:36 +1000

    MenuM

--- src/pages/layouts/projects/components/menuM.tsx
+++ src/pages/layouts/projects/components/menuM.tsx
@@ -7,16 +7,16 @@ import Idea from './Idea';
 const MenuM = () => {
   return (
     <div className="w-[95vw] h-full flex flex-col items-center center">
-      <div className="mt-[10vh] w-full h-[20vw] flex items-center ml-[15vw]">
+      <div className="mt-[10vh] w-full flex items-center ml-[15vw]">
         <Aim />
       </div>
-      <div className="w-full h-[60vw] flex items-center ml-[15vw]">
+      <div className="w-full flex items-center ml-[15vw]">
         <Complete />
       </div>
-      <div className="w-full h-[10vh] flex items-center ml-[15vw]">
+      <div className="w-full flex items-center ml-[15vw]">
         <Development />
       </div>
-      <div className="w-full h-[20vh] flex items-center ml-[15vw]">
+      <div className="w-full flex items-center ml-[15vw]">
         <Idea />
       </div>
     </div>
