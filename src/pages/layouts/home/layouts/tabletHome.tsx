8b4c6d934edb8f6520aa27fd3470c20a6adc6bf5
user <user@users.noreply.github.com>
2024-04-15 20:09:25 +1000

    tablet home

--- src/pages/layouts/home/layouts/tabletHome.tsx
+++ src/pages/layouts/home/layouts/tabletHome.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import WelcomeM from '../components/WelcomeM';
+import Welcome from '../components/Welcome';
 import InfoM from '../components/InfoM';
 
 const MobileHome = () => {
   return (
     <div className="flex flex-col w-full h-full">
-      <WelcomeM />
+      <Welcome />
       <InfoM />
     </div>
   );
