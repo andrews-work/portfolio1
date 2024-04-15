fff05a49b46a78c02749a2c593d838ac5d4720a9
user <user@users.noreply.github.com>
2024-04-15 23:45:42 +1000

    project layout for mobile

--- src/pages/layouts/home/components/Display.tsx
+++ src/pages/layouts/home/components/Display.tsx
@@ -5,7 +5,15 @@ const Display = () => {
     <div className="flex flex-col">
       {/* image */}
       <div className="h-[80vh] w-[75vw] flex items-center justify-center bg-[#f0f0f0] text-xl">
-        <h1>Your Website Here</h1>
+        <div className="group relative">
+            <h1 className="text-3xl text-black">Your Website Here</h1>
+            <div className="absolute invisible group-hover:visible z-10 w-[8vw] text-black rounded shadow-md px-3 py-2 text-sm right-0 mt-2">
+              <div>
+                <p className="leading-normal">Functionality coming soon</p>
+              </div>
+              <div className="absolute right-0 bottom-0 w-0 h-0 border-solid border-gray-900 border-t-0 border-r-0 border-b-2 border-l-2"></div>
+            </div>
+        </div>
       </div>
       {/* version */}
       <div className="h-[10vh] w-[75vw] border-t border-black flex items-center justify-center bg-[#e0e0e0] text-xl">
