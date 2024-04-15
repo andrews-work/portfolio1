435d8ca32418f5bad107ffad2ade1a08b68a5e91
user <user@users.noreply.github.com>
2024-04-15 18:35:44 +1000

    layout edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,16 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "it's technical",
   description: 'basic front end app',
-  icons: {
-    icon: '/favicon.ico',
-  },
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
