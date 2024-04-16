282a722a7b9cbf1df353679ba963d415215e65fb
user <user@users.noreply.github.com>
2024-04-16 18:46:52 +1000

    display projects

--- src/pages/layouts/projects/components/Display.tsx
+++ src/pages/layouts/projects/components/Display.tsx
@@ -8,8 +8,8 @@ interface DisplayProps {
 
 const Display: React.FC<DisplayProps> = ({ image, version }) => {
   return (
-    <div className="flex h-full w-[70vw] flex-col items-center mt-4">
-      <div className="h-auto">
+    <div className="flex h-[90vw] w-[70vw] flex-col items-center mt-4">
+      <div className="h-auto w-[50vw]">
         <Image src={image} alt="Project" layout="responsive" objectFit="contain" />
       </div>
       <div className="w-full max-w-[95vw] border-t border-black flex items-center justify-center text-xl py-2">
