acf05cb138d8418d9250bcdf7ce2e53d293e7db4
user <user@users.noreply.github.com>
2024-04-16 20:44:51 +1000

    project layout

--- src/pages/layouts/projects/components/Welcome.tsx
+++ src/pages/layouts/projects/components/Welcome.tsx
@@ -10,13 +10,12 @@ const Welcome: React.FC<projectProps> = ({ title, menu }) => {
   return (
     <div className="flex w-full h-[10vh] border-b border-black items-center">
         <div className="ml-[2vh]">
-          <Link href="/projects" className="no-underline">
+          <Link href="/projects" className="no-underline hover:underline">
             <h1 className="text-3xl text-black">{title}</h1>
           </Link>
         </div>
     </div>
   );
 };
-
 
 export default Welcome;
