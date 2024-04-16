2d996d41623e18735f501c4a281a40ba639e0140
user <user@users.noreply.github.com>
2024-04-16 18:57:35 +1000

    display projects

--- src/pages/layouts/projects/components/Display.tsx
+++ src/pages/layouts/projects/components/Display.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import Image from 'next/image';
 
 interface DisplayProps {
@@ -10,7 +10,7 @@ const Display: React.FC<DisplayProps> = ({ image, version }) => {
   return (
     <div className="flex h-[90vw] w-full items-center mt-4">
       <div className="h-auto w-full">
-        <Image src={image} alt="Project" layout="responsive" objectFit="contain" style={{ width: '100%', height: 'auto' }} />
+        <Image src={image} alt="Project" layout="responsive" objectFit="contain" style={{ width: '100%', height: 'auto' } as CSSProperties} />
       </div>
       <div className="w-full max-w-[95vw] border-t border-black flex items-center justify-center text-xl py-2">
         <p>{version}</p>
