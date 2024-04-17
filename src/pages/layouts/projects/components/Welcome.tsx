771c9f07d0f84150738a8ff412202f3bb814e018
user <user@users.noreply.github.com>
2024-04-17 10:31:18 +1000

    add welcome projects

--- src/pages/layouts/projects/components/Welcome.tsx
+++ src/pages/layouts/projects/components/Welcome.tsx
@@ -9,9 +9,9 @@ interface projectProps {
 const Welcome: React.FC<projectProps> = ({ title, menu }) => {
   return (
     <div>
-        <div className="">
+        <div className="bg-blue-500 h-[10vh] w-full border-black border-b">
           <Link href="/projects" className="no-underline hover:underline">
-            <h1 className="text-3xl text-black">{title} hi</h1>
+            <h1 className="text-3xl text-black">{title}</h1>
           </Link>
         </div>
     </div>
