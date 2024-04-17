21c0959969fea79e6ace62b153986c72ac0e9273
user <user@users.noreply.github.com>
2024-04-17 11:26:04 +1000

    add welcome projects

--- src/pages/layouts/projects/components/ProjectLayout.tsx
+++ src/pages/layouts/projects/components/ProjectLayout.tsx
@@ -36,7 +36,7 @@ const ProjectLayout: React.FC<ProjectLayoutProps> = ({ project }) => {
           return (
             <div className="h-full w-full flex flex-col">
               <div className="h-[10vh] w-full border-b border-black">
-                <Welcome title={project.title} menu={project.menu} />
+                {/* <Welcome title={project.title} menu={project.menu} /> */}
               </div>
               <div className="flex h-[90vh] w-full">
                 <div className="h-full w-[25vw] border-r border-black">
