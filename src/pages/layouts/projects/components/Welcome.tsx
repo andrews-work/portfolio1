e52a26449b0a9a7c2203a131dfae304a4b325bbf
user <user@users.noreply.github.com>
2024-04-22 17:15:52 +1000

    projects background

--- src/pages/layouts/projects/components/Welcome.tsx
+++ src/pages/layouts/projects/components/Welcome.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 interface projectProps {
@@ -7,11 +7,21 @@ interface projectProps {
 }
 
 const Welcome: React.FC<projectProps> = ({ title, menu }) => {
+  const [hoverTitle, setHoverTitle] = useState(title);
+
+  const handleMouseEnter = () => {
+    setHoverTitle('back');
+  };
+
+  const handleMouseLeave = () => {
+    setHoverTitle(title);
+  };
+
   return (
       <div className="h-[5vw] w-full flex items-center">
           <div className="ml-[5vw]">
-            <Link href="/projects" className="no-underline hover:underline" title="back">
-              <h1 className="text-3xl text-black mt-0">{title}</h1>
+            <Link href="/projects" className="no-underline hover:underline" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+              <h1 className="text-3xl text-black mt-0">{hoverTitle}</h1>
             </Link>
           </div>
           <div>
