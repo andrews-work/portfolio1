99432a9ab940d4157e12653506b4a4877e081d23
user <user@users.noreply.github.com>
2024-04-22 16:00:26 +1000

    projects background

--- src/pages/layouts/projects/components/ProjectLayout.tsx
+++ src/pages/layouts/projects/components/ProjectLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Welcome from './Welcome';
-// import Info from './Info';
-// import Display from './Display';
+import Info from './Info';
+import Display from './Display';
 import DeviceSize from '@/components/DeviceSize';
 import ProjectLayoutM from './ProjectLayoutM';
 
@@ -34,16 +34,14 @@ const ProjectLayout: React.FC<ProjectLayoutProps> = ({ project }) => {
           return project ? <ProjectLayoutM project={project} /> : <div>Project not found</div>;
         } else {
           return (
-            <div className="flex flex-col">
-              <div className="flex flex-row w-full h-[10vw] items-center border-b border-black bg-blue-500">
-                <div className="w-[90vw] flex justify-between">
-                  {/* <h1 className="">{project.title}</h1> */}
-                  {/* <h2 className="">{project.menu}</h2> */}
-                </div>
+            <div className="h-full w-full">
+              <div className="flex-col">
+              <div className="flex h-[10vh] w-full border-black border-b">
+                <Welcome title={project.title} menu={project.menu} />
               </div>
-              <div className="bg-red-100 flex">
-                <div className="w-[25vw] h-[90vh]">
-                  {/* <Info
+              <div className="flex h-[90vh] flex-row w-full">
+                <div className="w-[25vw] border-black border-r">
+                  <Info
                     aim1={project.aim1}
                     aim2={project.aim2}
                     aim3={project.aim3}
@@ -52,11 +50,12 @@ const ProjectLayout: React.FC<ProjectLayoutProps> = ({ project }) => {
                     process3={project.process3}
                     process4={project.process4}
                     code={project.code}
-                  /> */}
+                  />
                 </div>
-                <div className="w-[70vw] h-[90vh]">
-                  {/* <Display image={project.image} version={project.version} /> */}
+                <div className="w-[75vw]">
+                  <Display image={project.image} version={project.version} />
                 </div>
+              </div>
               </div>
             </div>
           );
