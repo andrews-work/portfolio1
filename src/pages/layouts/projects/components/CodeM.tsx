9f540eff3be470382955d2e40d089d39d22d617a
user <user@users.noreply.github.com>
2024-04-22 16:58:22 +1000

    projects background

--- src/pages/layouts/projects/components/CodeM.tsx
+++ src/pages/layouts/projects/components/CodeM.tsx
@@ -13,7 +13,7 @@ const CodeM: React.FC<CodeMProps> = ({ code }) => {
     }
   
     return (
-      <div className="w-full h-[20vh] mb-[2vh] flex-grow">
+      <div className="w-full mb-[2vh] flex-grow">
         <h1 className="text-3xl mb-[2vh]">Code</h1>
         <Link href={code} passHref>
           <a target="_blank" rel="noopener noreferrer" className="text-xl">
