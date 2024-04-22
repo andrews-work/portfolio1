c038b600f8ef8dbaa8930aac3cf295236f9c2bc9
user <user@users.noreply.github.com>
2024-04-22 16:41:46 +1000

    projects background

--- src/pages/layouts/projects/components/Welcome.tsx
+++ src/pages/layouts/projects/components/Welcome.tsx
@@ -8,7 +8,7 @@ interface projectProps {
 
 const Welcome: React.FC<projectProps> = ({ title, menu }) => {
   return (
-      <div className="h-[10vw] w-full flex items-center">
+      <div className="h-[10vw] w-full flex">
           <div className="ml-[5vw]">
             <Link href="/projects" className="no-underline hover:underline">
               <h1 className="text-3xl text-black">{title}</h1>
